26060d3c241bf5a08c555ed1e4b386a0b416dc2d
Eiko Oltmanns <eiko@maxr.org>
2022-04-23 16:30:41 +0200

    First try to create a new ressource: Sunflower field

--- data.lua
+++ data.lua
@@ -12,21 +12,82 @@ itemSubgroup.group = "bio-chemistry"
 data:extend{itemGroup, itemSubgroup}
 
 
-local stuff =  table.deepcopy(data.raw["item"]["iron-plate"])
-stuff.name = "stuff"
-stuff.subgroup = "biostuff1"
-stuff.icon = "__bio-chemistry__/stuff.png"
-stuff.icon_size = 32
+local sunflower =  table.deepcopy(data.raw["item"]["iron-plate"])
+sunflower.name = "sunflower"
+sunflower.subgroup = "biostuff1"
+sunflower.icon = "__bio-chemistry__/Sonnenblumenfeld.png"
+sunflower.icon_size = 64
 --TODO: order string?
 
-data:extend{stuff}
+data:extend{sunflower}
 
 
 local recipe = table.deepcopy(data.raw["recipe"]["heavy-armor"])
 recipe.enabled = true
 recipe.name = "stuff"
 recipe.ingredients = {{"iron-ore",10}}
-recipe.result = "stuff"
+recipe.result = "sunflower"
 
 
 data:extend{recipe}
+
+local resource = table.deepcopy(data.raw["resource"]["iron-ore"])
+resource.name = "sunflower"
+resource.icon = "__bio-chemistry__/Sonnenblumenfeld.png" --What is this for?
+resource.icon_size = 64
+resource.autoplace =
+{
+  control = "sunflower",
+  order = order,
+  sharpness = 15/16,
+  richness_multiplier = 1500,
+  richness_multiplier_distance_bonus = 20,
+  richness_base = 10,
+  coverage = 0.006 / 3,
+  peaks =
+  {
+    {
+  	noise_layer = "iron-ore",
+  	noise_octaves_difference = -0.85,
+  	noise_persistence = 0.4
+    }
+  },
+  starting_area_size = 5500 * 0.006 / 3,
+  starting_area_amount = 1600
+}
+resource.map_color = {1, 1, 0}
+resource.stage_counts = {0}
+resource.stages =
+    {
+      sheet =
+      {
+        filename = "__bio-chemistry__/Sonnenblumenfeld.png",
+        priority = "extra-high",
+        width = 64,
+        height = 64,
+        frame_count = 1,
+        variation_count = 1,
+      }
+	}
+resource.minable.result = "sunflower"
+
+-- Größe auf der Map bestimmen
+-- 64 Größe?
+-- Mining sound
+-- Walking sound
+-- Control Icon
+-- Mining Particle
+
+data:extend{resource}
+
+data:extend(
+{
+  {
+    type = "autoplace-control",
+    name = "sunflower",
+    richness = true,
+	can_be_disabled = true,
+    order = "z-a",
+    category = "resource"
+  }
+})
