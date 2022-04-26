38601057ed5d097ce809d8887eeccc54eea6b38e
Eiko Oltmanns <eiko@maxr.org>
2022-04-26 13:08:04 +0200

    Added ressource 'sugar cane field'

--- prototypes/resources.lua
+++ prototypes/resources.lua
@@ -146,6 +146,113 @@ data:extend(
 })
 
 --------------------------------
+-- sugar cane
+--------------------------------
+
+local resource = {
+  type = "resource",
+  name = prefix .. "sugar",
+  icon = "__bio-chemistry__/graphics/icons/Zuckerrohrfeld.png",
+  icon_size = 64,
+
+  collision_box = {{ -0.1, -0.1}, {0.1, 0.1}}, -- value taken from base mod
+  selection_box = {{ -0.5, -0.5}, {0.5, 0.5}}, -- full tile
+  flags = {"placeable-neutral"},
+
+  minable = 
+  {
+    mining_time = 2,
+    mining_particle = "green-asterisk-vegetation-particle-small-medium", 
+    results =
+    {
+      {
+        type = "item",
+        name = prefix .. "sugar-cane",
+        amount = 1
+      }
+    }
+  },
+
+  subgroup = prefix .. "resource",
+  remove_decoratives = true,
+  mining_sound = plantMinigSound,
+
+
+  autoplace =
+  {
+    control = prefix .. "sugar",
+    order = "d",
+    sharpness = 15/16,
+    richness_multiplier = 1500,
+    richness_multiplier_distance_bonus = 20,
+    richness_base = 10,
+    coverage = 0.004 / 3,
+    peaks =
+    {
+      {
+        noise_layer = prefix .. "sugar",
+        noise_octaves_difference = -0.85,
+        noise_persistence = 0.4
+      }
+    },
+    starting_area_size = 5500 * 0.006 / 3,
+    starting_area_amount = 1600
+  },
+
+  map_color = {0.37, 0.55, 0.14},
+  stage_counts = {0},
+  stages =
+  {
+    sheet =
+    {
+      filename = "__bio-chemistry__/graphics/entity/Zuckerrohrfeld.png",
+      priority = "extra-high",
+      width = 64,
+      height = 64,
+      frame_count = 8,
+      variation_count = 1,
+      hr_version = 
+      {
+        filename = "__bio-chemistry__/graphics/entity/hr-Zuckerrohrfeld.png",
+        priority = "extra-high",
+        width = 128,
+        height = 128,
+        frame_count = 8,
+        variation_count = 1,
+        scale = 0.5
+      }
+    }
+  },
+  
+  walking_sound = plantWalkingSound,
+  
+  tree_removal_probability = 1,
+  tree_removal_max_distance = 32 * 320,
+}
+data:extend{resource}
+
+data:extend(
+{
+  {
+    type = "noise-layer",
+    name = prefix .. "sugar"
+  }
+})
+
+data:extend(
+{
+  {
+    type = "autoplace-control",
+    name = prefix .. "sugar",
+    richness = true,
+	  can_be_disabled = true,
+    order = "z-a",
+    category = "resource",
+	  localised_name = {"", "[entity=" .. prefix .. "sugar] ", {"entity-name." .. prefix .. "sugar"}}
+  }
+})
+
+--------------------------------
 -- cows
 --------------------------------
 
